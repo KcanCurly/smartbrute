994d4dc60265e15b3b8259b312c9c28a7bed6e85
kcancurly <karadumankcan@gmail.com>
2026-03-24 11:44:53 +0300

    u

--- src/main.py
+++ src/main.py
@@ -255,7 +255,7 @@ def get_lockout_policy(server, base_dn, domain, user, password):
     }
 
 def get_lockout_policy_impacket(conn):
-    for i in conn.search("(objectClass=domain)", ['lockoutThreshold', 'lockoutDuration', 'lockoutObservationWindow', 'minPwdLength', 'pwdProperties', 'pwdHistoryLength']):
+    for i in conn.search("(objectClass=domain)", ['lockoutthreshold', 'lockoutduration', 'lockoutobservationwindow', 'minpwdlength', 'pwdproperties', 'pwdhistorylength']):
         print(i)
     
 
